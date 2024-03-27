379b22229123587d949ddbf1a9e366b0d4ff7aec
I337592 <parisanoushazaran@gmail.com>
2024-03-27 19:22:25 +0000

    all tests passed

--- src/main/java/org/example/FindTheOddInt.java
+++ src/main/java/org/example/FindTheOddInt.java
@@ -1,9 +1,22 @@
 package org.example;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class FindTheOddInt {
 
-    public int findTheOddInt(int a) {
-        return a;
+    public int findTheOddInt(int[] a) {
+        Map<Integer,Integer> countOfIntegers = new HashMap<>();
+        for (int i = 0; i < a.length; i++) {
+            countOfIntegers.put(a[i], countOfIntegers.getOrDefault(a[i],0)+1);
+        }
+
+        for (Map.Entry<Integer, Integer> entry : countOfIntegers.entrySet()) {
+              if(entry.getValue()%2 != 0) {
+                  return entry.getKey();
+              }
+        }
+        return 0;
     }
 
 }
--- src/test/java/FindTheOddIntTest.java
+++ src/test/java/FindTheOddIntTest.java
@@ -5,6 +5,33 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class FindTheOddIntTest {
     @Test void Returns7() {
         FindTheOddInt findtheoddint = new FindTheOddInt();
-        assertEquals(findtheoddint.findTheOddInt(7), 7);
+        int[] OddArray = {7};
+        assertEquals(findtheoddint.findTheOddInt(OddArray), 7);
     }
+
+    @Test void Returns0() {
+        FindTheOddInt findtheoddint = new FindTheOddInt();
+        int[] OddArray = {0};
+        assertEquals(findtheoddint.findTheOddInt(OddArray), 0);
+    }
+
+    @Test void Returns2() {
+        FindTheOddInt findtheoddint = new FindTheOddInt();
+        int[] OddArray = {1,1,2};
+        assertEquals(findtheoddint.findTheOddInt(OddArray), 2);
+    }
+
+    @Test void Returns0ForList() {
+        FindTheOddInt findtheoddint = new FindTheOddInt();
+        int[] OddArray = {0,1,0,1,0};
+        assertEquals(findtheoddint.findTheOddInt(OddArray), 0);
+    }
+
+    @Test void Returns4ForList() {
+        FindTheOddInt findtheoddint = new FindTheOddInt();
+        int[] OddArray = {1,2,2,3,3,3,4,3,3,3,2,2,1};
+        assertEquals(findtheoddint.findTheOddInt(OddArray), 4);
+    }
+
+
 }
